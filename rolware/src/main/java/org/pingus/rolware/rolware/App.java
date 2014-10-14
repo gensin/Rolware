06d96dfbb5b8dee0caa8009a8dfdccb0a5e0b8ce
Pau Arlandis Martinez <ghostdesert@gmail.com>
2014-10-14 23:56:14 +0200

    initial

---
+++ rolware/src/main/java/org/pingus/rolware/rolware/App.java
@@ -0,0 +1,13 @@
+package org.pingus.rolware.rolware;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
